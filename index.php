c07ab3bf00db9107bea4b928dad2dd948423a8b2
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-15 15:43:50 +0200

    scaffholding

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+</head>
+<body>
+        <!-- Header -->
+    <?php include_once __DIR__ . "/partials/header.php"; ?>
+
+
+        <!-- Main -->
+    <?php include_once __DIR__ . "/partials/header.php"; ?>
+    
+    
+</body>
+</html>
