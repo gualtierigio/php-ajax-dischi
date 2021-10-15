d1e7b9187bfb290afec0af61dcc543175f26fb23
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-15 17:53:40 +0200

    brutto ma funziona

--- partials/main.php
+++ partials/main.php
@@ -5,6 +5,13 @@ include __DIR__ . "/../db/database.php";
 
  ?>
 
- <h1>
-     prova
- </h1>
+<div>
+    <?php foreach($movieDatas as $disco){; ?>
+
+        <h1>Titolo: <?php echo $disco['title'] ; ?></h1>
+        <p>Autore e genere: <?php echo $disco['author'] . ", " . $disco['genre'] ; ?> </p>
+        <p>Anno: <?php echo $disco['year'] ; ?></p>
+        
+
+    <?php }; ?>
+</div>
